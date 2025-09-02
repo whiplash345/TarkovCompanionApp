830d44712797eb125051df2e416ae0a8efcc0768
Noah Bisping <NEBisping@student.fullsail.edu>
2025-09-02 11:38:20 -0400

    Environment setup version 1

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,3 +2,4 @@ import { createClient } from '@supabase/supabase-js';
 const url = process.env.EXPO_PUBLIC_SUPABASE_URL!;
 const anon = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!;
 export const supabase = createClient(url, anon);
+console.log('SB URL', process.env.EXPO_PUBLIC_SUPABASE_URL);
